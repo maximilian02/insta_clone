d662cc2fd729c690ce208d9ac5801481233cf7fe
Max Zelarayán <max.z_@hotmail.com>
2023-04-15 10:12:03 -0300

    Saving progress managing sessions and some home screen work

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -17,11 +17,29 @@ const Post: React.FC<PostType> = ({ likedBy, uploadDate, description, id }) => {
     setIsLiked(e.target.checked)
     setLikesAmount(newAmount)
   }
-  console.log('user', user)
 
   return <>
     <div className="w-1/3 flex flex-row">
-      <div className="flex flex-col p-4">
+      <div className="w-full flex flex-col mb-8">
+        <figure className="relative transition-all m-auto pl-4">
+          <img
+            className="rounded-lg"
+            src={`https://picsum.photos/300/300?random=${id}`}
+            alt="insta clone random image"
+          />
+          <figcaption className="w-20 absolute bg-white border border-black text-lg text-white bottom-0 right-0 py-0 rounded-lg">
+            <input type="checkbox" className="mx-2 inline" checked={isLiked} onChange={changeHandler} /><p className="inline text-black w-auto">LIKE</p>
+          </figcaption>
+        </figure>
+        <div className="w-80 text-gray-500 flex flex-row text-xs justify-between m-auto mt-2 pl-4">
+          <div><p>{uploadDate}</p></div>
+          <div><p>{likesAmount} Likes</p></div>
+        </div>
+        <div className="w-80 text-gray-500 flex flex-row justify-between left m-auto mt-2 pl-4">
+          <div><p>{description}</p></div>
+        </div>
+      </div>
+      {/* <div className="flex flex-col p-4">
         <img src={`https://picsum.photos/250/250?random=${id}`} />
         <div className="flex flex-row w-2/4">
           <div className="absolute bg-gray-400 w-1/12 -mt-8">
@@ -30,8 +48,7 @@ const Post: React.FC<PostType> = ({ likedBy, uploadDate, description, id }) => {
         </div>
         <div className="flex flex-row w-full">
           <span>{likesAmount}</span>
-        </div>
-      </div>
+        </div> */}
     </div>
   </>
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,48 +6,50 @@ import Button from '../components/Button'
 import { useNavigate } from 'react-router-dom'
 import Post, { PostType } from '../components/Post'
 
+const DUMMY_POSTS: PostType[] = [
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": ["maxzelarayan"],
+    "id": 1
+  },
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": [],
+    "id": 2
+  },
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": ["maxzelarayan"],
+    "id": 3
+  },
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": ["maxzelarayan"],
+    "id": 4
+  },
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": [],
+    "id": 5
+  },
+  {
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "uploadDate": "2 minutes ago",
+    "likedBy": ["masi"],
+    "id": 6
+  }
+]
+
 const Home: React.FC = () => {
-  const posts: PostType[] = [
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: ['masi'],
-      id: 1
-    },
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: [],
-      id: 2
-    },
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: ['masi'],
-      id: 3
-    },
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: ['masi'],
-      id: 1
-    },
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: [],
-      id: 2
-    },
-    {
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      uploadDate: '2 minutes ago',
-      likedBy: ['masi'],
-      id: 3
-    }
-  ]
+  const feed = [...DUMMY_POSTS]
   const { username } = DB.getSessionData()
   const navigate = useNavigate()
-  const containerHeadClass = 'flex flex-row flex-wrap content-center bg-gray-200 p-3 lg:w-11/12 w-10/12'
+  const containerHeadClass = 'flex flex-row flex-wrap justify-between bg-gray-200 p-6 lg:w-11/12 w-8/12'
   const containerImagesClass = 'flex flex-row justify-center flex-wrap content-center bg-gray-200 p-3 lg:w-11/12 w-10/12'
 
   const logOutHandler = () => {
@@ -59,9 +61,11 @@ const Home: React.FC = () => {
   return <>
     <FormLayout>
       <div className={containerHeadClass}>
-        <div className="w-8/12 p-2 flex flex-row justify-center items-center flex-wrap border border-black">
-          <p className="text-6xl">&#43;</p>
-          <p className="ml-4 mt-2 text-2xl">Drag image here to post it</p>
+        <div className="w-7/12 p-2 flex flex-row justify-center items-center flex-wrap border border-black">
+          <div className="w-6/12 flex flex-row justify-between items-center flex-wrap">
+            <p className="text-6xl">&#43;</p>
+            <p className="ml-4 mt-2 text-2xl">Drag image here to post it</p>
+          </div>
         </div>
         <div className="w-4/12 p-2 flex flex-col flex-wrap">
           <div className="w-full flex flex-row justify-between items-center">
@@ -77,7 +81,7 @@ const Home: React.FC = () => {
         </div>
       </div>
       <div className={containerImagesClass}>
-        {posts.map(({ likedBy, uploadDate, description }, index) =>
+        {feed.map(({ likedBy, uploadDate, description }, index) =>
           <Post likedBy={likedBy} uploadDate={uploadDate} id={index} description={description} />)
         }
       </div>
